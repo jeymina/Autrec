5268d445d6a0464fb0b0958700eb342a49b7494b
Pitipouet <sebast.ruelle@gmail.com>
2017-03-29 21:58:53 +0200

    rectif des appels au listes de plats

--- projet Restaurant/home.js
+++ projet Restaurant/home.js
@@ -7,9 +7,7 @@ monApp.controller('appCtrl', ['$scope', '$animate', '$rootScope', '$http', '$loc
     
     $scope.init = function() {
         $scope.checkIfConnected();
-        $scope.getListeEntrees();
         $scope.getListePlats();
-        $scope.getListeDesserts();
     }
 
     $scope.redirect = function() {
@@ -99,10 +97,10 @@ monApp.controller('appCtrl', ['$scope', '$animate', '$rootScope', '$http', '$loc
         }
     }
 
-    $scope.getListeEntrees = function() {
-        console.log("entrée dans le getListeEntrees");
+    $scope.getListePlats = function() {
+        console.log("entrée dans le getListePlats");
         $http({
-            url: "http://25.66.6.53:8080/restokevina/lesentrees.htm",
+            url: "http://25.66.6.53:8080/restokevina/lesplatstrie.htm",
             method: "GET",
             withCredentials: true,
             headers: {
@@ -112,45 +110,7 @@ monApp.controller('appCtrl', ['$scope', '$animate', '$rootScope', '$http', '$loc
             if (data.data.response.retour === "success") {
                 console.log(data.data);
                 $scope.listeEntrees = data.data.response.listeEntree;
-            }
-        }).catch(function(data) {
-            console.log("GetSession de l'index n'est pas passé");
-        });
-
-    }
-
-    $scope.getListePlats = function() {
-        console.log("entrée dans le getListePlats");
-        $http({
-            url: "http://25.66.6.53:8080/restokevina/lesplatsprinc.htm",
-            method: "GET",
-            withCredentials: true,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function(data) {
-            if (data.data.response.retour === "success") {
-                console.log(data.data);
                 $scope.listePlats = data.data.response.listePlatPrinc;
-            }
-        }).catch(function(data) {
-            console.log("GetSession de l'index n'est pas passé");
-        });
-
-    }
-
-    $scope.getListeDesserts = function() {
-        console.log("entrée dans le getListeDesserts");
-        $http({
-            url: "http://25.66.6.53:8080/restokevina/lesdessert.htm",
-            method: "GET",
-            withCredentials: true,
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }).then(function(data) {
-            if (data.data.response.retour === "success") {
-                console.log(data.data);
                 $scope.listeDesserts = data.data.response.listeDessert;
             }
         }).catch(function(data) {
