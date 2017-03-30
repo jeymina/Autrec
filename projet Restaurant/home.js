d4c1cdc01c8ffccd2b08ae2f46e896223bdb5e5d
Unknown <colin.jerome29@gmail.com>
2017-03-30 11:01:38 +0200

    gérage quantité

--- projet Restaurant/home.js
+++ projet Restaurant/home.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
     var monApp = angular.module('monAppli', ['ngRoute', 'ngAnimate', 'ngCookies']);
 
     monApp.controller('appCtrl', ['$scope', '$animate', '$rootScope', '$http', '$location', function($scope, $animate, $rootScope, $http, $location) {
@@ -52,6 +53,89 @@
             }).then(function(data) {
                 if (data.data.response.retour === "success") {
                     console.log("déconnexion effectuée")
+=======
+var monApp = angular.module('monAppli', ['ngRoute', 'ngAnimate', 'ngCookies']);
+
+monApp.controller('appCtrl', ['$scope', '$animate', '$rootScope', '$http', '$location', function($scope, $animate, $rootScope, $http, $location) {
+    var vm = this;
+    console.log("coucou");
+    console.log();
+    
+    $scope.init = function() {
+        $scope.checkIfConnected();
+        $scope.getListePlats();
+    }
+
+    $scope.redirect = function() {
+        $location.path('/login');
+    }
+
+    $scope.ajouterAuPanier = function(id){
+        console.log("ajouterAuPanier"+id);
+        $http({
+            url: "http://25.66.6.53:8080/restokevina/ajoutepanier.htm",
+            method: "POST",
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {
+                platId: id,
+                userId: $scope.connected.data.response.session.utilActif.id,
+                qte: document.getElementById("searchTxt").value
+            }
+        }).then(function(data) {
+            if (data.data.response.retour === "success") {         
+            }
+        }).catch(function(data) {
+            console.log("ajout pas fait");
+        });
+    }
+
+
+    $scope.logout = function() {
+        $http({
+            url: "http://25.66.6.53:8080/restokevina/deconnexion.htm",
+            method: "POST",
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            data: {}
+        }).then(function(data) {
+            if (data.data.response.retour === "success") {
+                console.log("déconnexion effectuée")
+                $scope.connected = null;
+                //[].forEach.call(document.querySelectorAll('.navbar'), function (el) {
+                  //  el.style.visibility = 'hidden';
+               // });          
+            }
+        }).catch(function(data) {
+            console.log("déconnexion pas effectuée");
+        });
+    }
+
+    $scope.checkIfConnected = function() {
+        console.log("entrée dans le checkIfConnected");
+        $http({
+            url: "http://25.66.6.53:8080/restokevina/getsession.htm",
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(data) {
+            if (data.data.response.retour === "success") {
+                console.log(data.data);
+                if (data.data.response.session.utilActif !== null) {
+                    $scope.connected = data;
+                    console.log("data" + data.data);
+                    console.log("Je suis connecté :" + data.data.response.session.utilActif.nom);
+                    console.log($scope.connected.data.response.session.utilActif.nom);
+                    console.log($scope.connected);
+                } else {
+                    console.log("je met $scope.connected à null (index)")
+>>>>>>> Stashed changes
                     $scope.connected = null;
                     //[].forEach.call(document.querySelectorAll('.navbar'), function (el) {
                     //  el.style.visibility = 'hidden';
@@ -89,7 +173,31 @@
                 console.log("GetSession de l'index n'est pas passé");
             });
 
+<<<<<<< Updated upstream
         }
+=======
+
+
+    $scope.getListePlats = function() {
+        console.log("entrée dans le getListePlats");
+        $http({
+            url: "http://25.66.6.53:8080/restokevina/lesplatstrie.htm",
+            method: "GET",
+            withCredentials: true,
+            headers: {
+                'Content-Type': 'application/json'
+            }
+        }).then(function(data) {
+            if (data.data.response.retour === "success") {
+                console.log(data.data);
+                $scope.listeEntrees = data.data.response.listeEntree;
+                $scope.listePlats = data.data.response.listePlatPrinc;
+                $scope.listeDesserts = data.data.response.listeDessert;
+            }
+        }).catch(function(data) {
+            console.log("GetSession de l'index n'est pas passé");
+        });
+>>>>>>> Stashed changes
 
         $scope.getPanier = function() {
             console.log("entrée dans le getPanier");
